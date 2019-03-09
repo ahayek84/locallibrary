124867e45fb295f1100f8f86a531594412daf5dc
Abdullrahman ElHayek <ahayek84@gmail.com>
2019-03-09 18:51:35 +0200

    Register working adding proxy model to user so that more features can be added

--- authenticate/models.py
+++ authenticate/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class User(User):
+    email = User.email
+    first_name = User.first_name
+    last_name = User.last_name
+    fav_colour = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('first_name', )
+
+
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -30,3 +31,20 @@ def logout_user(request):
         return redirect('home')
     else:
         return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request,('you have successfuly register it'))
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+    context = {'form':form}
+    return render(request, 'authenticate/register.html', context)
