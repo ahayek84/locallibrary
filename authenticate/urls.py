80a03b255810ad5161ce8c580c511095fad6eefc
Abdullrahman ElHayek <ahayek84@gmail.com>
2019-03-09 16:33:33 +0200

    Login form worked

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -17,7 +17,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('login/',views.login_user,name='login'),
 ]
 
 #urlpatterns = [
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     return render(request,'authenticate/home.html',{})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,('you have successfuly logged it'))
+            return redirect('home')
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request, ('login failed'))
+            return redirect('login')
+    else:
+        return render(request,'authenticate/login.html',{},)
