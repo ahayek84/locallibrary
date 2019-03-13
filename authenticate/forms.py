c7fc21cf9a39eacfdf26125d9609182a47e3de1c
Abdullrahman ElHayek <ahayek84@gmail.com>
2019-03-13 14:52:39 +0200

    style the form with widget

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -4,10 +4,17 @@ from django import forms
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    fav_colour = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    fav_colour = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username','first_name','last_name','email','password1','password2','fav_colour')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
